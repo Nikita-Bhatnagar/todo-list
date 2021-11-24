b5a9e3d1ed94aa93e248ed8185c9a6aa3a3e579e
Nikita Bhatnagar <bhatnagarnikita.02@gmail.com>
2021-11-24 18:36:48 +0530

    filter functionality added

--- script.js
+++ script.js
@@ -2,21 +2,28 @@
 const newTodoContent = document.querySelector(".new-todo-content");
 const newTodoBtn = document.querySelector(".new-todo .btn-unchecked");
 const todoList = document.querySelector(".list");
-const deleteBtn = document.getElementsByClassName("btns");
+let checkbtn = document.querySelectorAll(".btn-unchecked");
 const editWindow = document.querySelector(".edit-window");
 const edit = document.querySelector(".edit");
 const closebtn = document.querySelector(".close");
 const editedTodo = document.querySelector(".newTodo");
+const completionStatus = document.querySelector(".comp-status");
+const todoItems = document.querySelectorAll(".todo-item");
+const themeToggler = document.querySelector(".theme-toggler");
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const createNewTodo = document.querySelector(".new-todo");
+const divTodoList = document.querySelector(".todo-list");
 
 //event listeners
 newTodoBtn.addEventListener("click", addTodo);
 window.addEventListener("DOMContentLoaded", displayTodos);
-
 todoList.addEventListener("mouseover", displayCross);
 todoList.addEventListener("mouseout", hideCross);
 todoList.addEventListener("click", removeOrEdit);
-
+completionStatus.addEventListener("click", filter);
 closebtn.addEventListener("click", closeEditWindow);
+themeToggler.addEventListener("click", changeTheme);
 
 //functions
 
@@ -25,7 +32,7 @@ function addTodo(e) {
 <div class="todo">
  <div class="btnAndWork">
   <div>
-    <button type="button" class="btn-unchecked"></button>
+    <button type="button" class="btn-unchecked check-button"></button>
   </div>
   <div>
   <span class="work">${newTodoContent.value}</span>
@@ -60,6 +67,8 @@ function addTodo(e) {
   localStorage.setItem("todosList", JSON.stringify(todosList));
 
   newTodoContent.value = "";
+  currentTheme();
+  hideStatus();
 }
 function displayTodos(e) {
   e.preventDefault();
@@ -73,7 +82,7 @@ function displayTodos(e) {
 <div class="todo">
  <div class="btnAndWork">
   <div>
-    <button type="button" class="btn-unchecked"></button>
+    <button type="button" class="btn-unchecked check-button"></button>
   </div>
   <div>
   <span class="work">${task}</span>
@@ -100,6 +109,7 @@ function displayTodos(e) {
 </li>`;
     todoList.insertAdjacentHTML("afterbegin", todo);
   });
+  hideStatus();
 }
 
 function displayCross(e) {
@@ -119,16 +129,24 @@ function hideCross(e) {
 var parentitem;
 function removeOrEdit(e) {
   parentitem = e.target.closest(".todo");
+
   let todosList;
   todosList = JSON.parse(localStorage.getItem("todosList"));
 
   let index = todosList.indexOf(parentitem.querySelector(".work").innerText);
+
   if (e.target.classList.contains("dlt-btn")) {
     todosList.splice(index, 1);
     localStorage.setItem("todosList", JSON.stringify(todosList));
     e.target.closest(".todo-item").style.display = "none";
+    e.target.closest(".todo-item").remove();
+
+    hideStatus();
   } else if (e.target.classList.contains("edit-btn")) {
     editWindow.style.zIndex = "1";
+  } else if (e.target.classList.contains("check-button")) {
+    e.target.classList.toggle("checked");
+    parentitem.querySelector(".work").classList.toggle("done");
   }
   edit.addEventListener("click", editing(parentitem));
   function editing(parentitem) {
@@ -144,4 +162,72 @@ function removeOrEdit(e) {
 
 function closeEditWindow(e) {
   editWindow.style.zIndex = "-1";
+}
+function filter(e) {
+  hideStatus();
+
+  let todos = todoList.childNodes;
+  todos.forEach((todo) => {
+    switch (e.target.classList[0]) {
+      case "all":
+        todo.style.display = "unset";
+
+        break;
+      case "active":
+        todo.style.display = "unset";
+        if (todo.querySelector(".done") !== null) todo.style.display = "none";
+        break;
+      case "finished":
+        todo.style.display = "unset";
+        if (todo.querySelector(".done") === null) todo.style.display = "none";
+        break;
+    }
+  });
+}
+function hideStatus() {
+  if (todoList.children[0] === undefined) {
+    completionStatus.classList.add("hidden");
+  } else completionStatus.classList.remove("hidden");
+}
+hideStatus();
+let curTheme = "dark";
+function changeTheme(e) {
+  checkbtn = document.querySelectorAll(".btn-unchecked");
+  if (curTheme === "dark") {
+    themeToggler.src = "images/icon-moon.svg";
+    body.style.backgroundColor = "hsl(0, 0%, 98%)";
+    body.style.color = "hsl(235, 19%, 35%)";
+    container.style.backgroundImage = "url(images/bg-desktop-light.jpg)";
+    newTodoContent.style.backgroundColor = "hsl(0, 0%, 98%)";
+    newTodoContent.style.color = "hsl(235, 19%, 35%)";
+    createNewTodo.style.backgroundColor = "hsl(0, 0%, 98%)";
+    divTodoList.style.backgroundColor = "hsl(0, 0%, 98%)";
+    divTodoList.style.boxShadow = "1px 1px 10px 10px hsl(236, 33%, 95%)";
+    checkbtn.forEach((btn) => {
+      btn.style.backgroundColor = "hsl(0, 0%, 98%)";
+    });
+    curTheme = "light";
+  } else {
+    themeToggler.src = "images/icon-sun.svg";
+    body.style.backgroundColor = "hsl(235, 21%, 11%)";
+    body.style.color = "hsl(234, 39%, 85%)";
+    container.style.backgroundImage = "url(images/bg-desktop-dark.jpg)";
+    newTodoContent.style.backgroundColor = "hsl(235, 24%, 19%)";
+    newTodoContent.style.color = "hsl(234, 39%, 85%)";
+    createNewTodo.style.backgroundColor = "hsl(235, 24%, 19%)";
+    divTodoList.style.backgroundColor = "hsl(235, 24%, 19%)";
+    divTodoList.style.boxShadow = "1px 1px 10px 10px hsl(235, 27%, 10%)";
+    checkbtn.forEach((btn) => {
+      btn.style.backgroundColor = "hsl(235, 24%, 19%)";
+    });
+    curTheme = "dark";
+  }
+}
+function currentTheme() {
+  checkbtn = document.querySelectorAll(".btn-unchecked");
+  if (curTheme === "light") {
+    checkbtn.forEach((btn) => {
+      btn.style.backgroundColor = "hsl(0, 0%, 98%)";
+    });
+  }
 }
